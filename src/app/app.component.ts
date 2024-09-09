a025e8ed0dfc849278997ea0a45e13b85301a955
Abdul Sameeu <abdulsameeu26@gmail.com>
2024-09-09 11:27:38 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,PasswordGeneratorComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+}
---
+++ src/app/password-generator/password-generator.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-password-generator',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './password-generator.component.html',
+  styleUrl: './password-generator.component.css'
+})
+export class PasswordGeneratorComponent {
+  password: string ='Click on generate';
+  length:number=12;
+  includeUpperCase:boolean=true;
+  includeNumbers:boolean=true;
+  includeSpecial:boolean=true;
+  
+  generatepassword(){
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';  
+
+    let charSet=lowercaseChars;
+    if(this.includeUpperCase){
+      charSet+=uppercaseChars;
+    }
+
+    if (this.includeNumbers) {
+      charSet += numberChars;
+    }
+    
+    if (this.includeSpecial) {
+      charSet += specialChars;
+    }
+    
+    this.password = '';
+    for (let i = 0; i < this.length; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      this.password += charSet[randomIndex];
+    }
+  }
+
+}
